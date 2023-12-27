6290501389f92dbcb55f468aca719eed4d4a7cf6
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-26 21:55:33 -0500

    Animate the logo and the  SVG

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 let gamepad = document.querySelector("#layer_1");
+let userInput = document.querySelector("#login");
 
-gamepad.style.transform = 'rotate(34deg)';
+let totalRotation = 0;
+
+function inputTrigger() {
+    totalRotation += 5;
+    gamepad.style.transform = `translateX(350px) translateY(400px) scale(5) rotate(${totalRotation}deg)`;
+    gamepad.style.transition = 'transform 2s';
+
+    if(userInput.value == ''){
+	totalRotation = 0;
+	gamepad.style.transform = `rotate(${totalRotation}deg)`;
+    }
+}
+
+userInput.addEventListener('input', inputTrigger);
