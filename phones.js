7c146524923a1853e7f1a3bd2f7d432e5c74d482
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-28 21:59:45 -0500

    Make the animations  smoother

--- phones.js
+++ phones.js
@@ -1,17 +1,22 @@
+if (window.innerWidth <= 600) {
 let gameP = document.querySelector("#layer_1");
-let userInp = document.querySelector("#login");
+let userInp = document.querySelectorAll("input");
 
 let totalRot = 0;
 
 function inputTrigger() {
 	totalRot += 5;
-    gameP.style.transform = `translateX(60px) translateY(400px) scale(2) rotate(${totalRotation}deg)`;
+    gameP.style.transform = `translateX(60px) translateY(400px) scale(2) rotate(${totalRot}deg)`;
     gameP.style.transition = 'transform 2s';
-
+    userInp.forEach(input =>{
     if(userInp.value == ''){
 	totalRot = 0;
 	gameP.style.transform = `rotate(${totalRot}deg)`;
     }
+})
 }
 
-userInp.addEventListener('input', inputTrigger);
+userInp.forEach(input=>{
+    input.addEventListener('input', inputTrigger)
+    })
+}
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let gamepad = document.querySelector("#layer_1");
-let userInput = document.querySelector("#login");
+let userInput = document.querySelectorAll('input');
 
 let totalRotation = 0;
 
@@ -7,11 +7,14 @@ function inputTrigger() {
     totalRotation += 5;
     gamepad.style.transform = `translateX(350px) translateY(400px) scale(5) rotate(${totalRotation}deg)`;
     gamepad.style.transition = 'transform 2s';
-
+    userInput.forEach(input =>{
     if(userInput.value == ''){
 	totalRotation = 0;
 	gamepad.style.transform = `rotate(${totalRotation}deg)`;
     }
+})
 }
 
-userInput.addEventListener('input', inputTrigger);
+userInput.forEach(input=>{
+input.addEventListener('input', inputTrigger)
+})
