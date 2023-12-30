ce5623e2ff6586de2c429a314beebfcc48b6127e
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2023-12-29 20:37:46 -0500

    Adjust speed of the animated logo

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let gamepad = document.querySelector("#realPad");
 let userInput = document.querySelectorAll('input');
 let gameDiv = document.getElementById('gamepad')
 let odin = document.getElementById('odin')
+let FPS = 60;
 
 let totalRotation = 0;
 
 let xPosition = 5;
 let yPosition = 5;
-let xSpeed = 10;
-let ySpeed = 10;
+let xSpeed = 5;
+let ySpeed = 5;
 
 function update(){
     odin.style.left = xPosition + 'px'
@@ -18,10 +19,17 @@ function update(){
 
 
 setInterval(()=>{
+    if(xPosition + odin.clientWidth >= gameDiv.clientWidth ||
+        xPosition <= 0 ){
+        xSpeed = -xSpeed;
+    } 
+    if (yPosition + odin.clientHeight >= gameDiv.clientHeight || yPosition <= 0){
+            ySpeed = -ySpeed;
+    }
     xPosition += xSpeed;
     yPosition += ySpeed;
-    // update()
-},1000)
+    update()
+},1000/FPS)
 
 
 
